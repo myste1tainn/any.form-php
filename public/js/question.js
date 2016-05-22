69adb2a1af20de8301add6087bec66470a5d51fa
A. Keereena <arnon@invp.co.th>
2016-05-22 14:32:56 +0700

    Add reactive like Collections (service) of Questions and QuestionGroup

--- public/js/question.js
+++ public/js/question.js
@@ -2,6 +2,128 @@
 	
 	var module = angular.module('question', [])
 
+	.service('Questions', function($http, ArrayHelper){
+		var _collections = [];
+		var _subscribers = [];
+
+		var _getCollections = function(id) {
+			var _success = function(res, status, headers, config){
+				if (typeof res == 'object' || typeof res == 'array') {
+					for (var i = res.length - 1; i >= 0; i--) {
+						_collections.push(res[i]);
+					}
+				}
+			};
+			var _error = function(res, status, headers, config){
+				console.log(res);
+				_collections = [];
+			}
+			
+			if (id === undefined) {
+				$http.get('api/v1/questions')
+				.success(_success)
+				.error(_error);
+			} else {
+				$http.get('api/v1/form/'+id+'/questions')
+				.success(_success)
+				.error(_error);
+			}
+		}
+
+		this.constructor = function() {
+			_getCollections();
+		}
+		this.all = function(formID) {
+			if (formID !== undefined) {
+				_getCollections(formID);
+			}
+			return _collections;
+		}
+		this.insert = function(form, group) {
+			$http.post('api/v1/form/'+form.id+'/question', group)
+			.success(function(res, status, headers, config){
+				_collections.push(res);
+			})
+			.error(function(res, status, headers, config){
+				console.log(res);
+			});
+		}
+		this.update = function(form, group) {
+			$http.put('api/v1/form/'+form.id+'/question', group)
+			.success(function(res, status, headers, config){
+				console.log(res);
+			})
+			.error(function(res, status, headers, config){
+				console.log(res);
+			});
+		}
+		this.remove = function(payload) {
+			$http.delete('api/v1/question/'+payload.id)
+			.success(function(res, status, headers, config){
+				_collections = ArrayHelper.remove(payload, _collections);
+			})
+			.error(function(res, status, headers, config){
+				//Code
+			});
+		}
+
+
+		this.constructor();
+	})
+
+	.service('Groups', function($http, ArrayHelper){
+		var _collections = [];
+		var _subscribers = [];
+
+		this.constructor = function() {
+			$http.get('api/v1/question-groups')
+			.success(function(res, status, headers, config){
+				if (typeof res == 'object' || typeof res == 'array') {
+					for (var i = res.length - 1; i >= 0; i--) {
+						_collections.push(res[i]);
+					}
+				}
+			})
+			.error(function(res, status, headers, config){
+				console.log(res);
+				_collections = [];
+			});
+		}
+		this.all = function(group) {
+			return _collections;
+		}
+		this.insert = function(group) {
+			$http.post('api/v1/question-group', group)
+			.success(function(res, status, headers, config){
+				_collections.push(res);
+			})
+			.error(function(res, status, headers, config){
+				console.log(res);
+			});
+		}
+		this.update = function(group) {
+			$http.put('api/v1/question-group', group)
+			.success(function(res, status, headers, config){
+				console.log(res);
+			})
+			.error(function(res, status, headers, config){
+				console.log(res);
+			});
+		}
+		this.remove = function(payload) {
+			$http.delete('api/v1/question-group/'+payload.id)
+			.success(function(res, status, headers, config){
+				_collections = ArrayHelper.remove(payload, _collections);
+			})
+			.error(function(res, status, headers, config){
+				//Code
+			});
+		}
+
+
+		this.constructor();
+	})
+
 	.service('$question', function($choice){
 		this.newInstance = function() {
 			return {
