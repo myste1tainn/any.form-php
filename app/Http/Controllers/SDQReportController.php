6659494a6845953d4018da494bf6ef6653201612
Arnon Keereena <a.keereena@gmail.com>
2016-05-30 18:12:29 +0700

    Add support for Class & School level for SDQ

--- app/Http/Controllers/SDQReportController.php
+++ app/Http/Controllers/SDQReportController.php
@@ -24,6 +24,18 @@ class SDQReportController extends Controller {
 	}
 
 	public function resultByRoom($id, $class, $room, $year) {
+		return $this->resultByType($id, $class, $room, $year);
+	}
+
+	public function resultByClass($id, $class, $year) {
+		return $this->resultByType($id, $class, null, $year);
+	}
+
+	public function resultBySchool($id, $year) {
+		return $this->resultByType($id, null, null, $year);
+	}
+
+	private function resultByType($id, $class = null, $room = null, $year) {
 		$participants = Questionaire::participantsForQuestionaire($id, $year, 0, 1000000, $class, $room);
 		$questionaire = Questionaire::with('questionGroups.questions', 'questionGroups.criteria')
 									->where('id', $id)
