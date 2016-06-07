170edde04730256093de6c29c78f121fc3a4a391
A. Keereena <arnon@invp.co.th>
2016-06-07 23:48:28 +0700

    Add theme to navigation sidebar & form list page

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default col-xs-3 col-lg-2">
+<nav class="navbar navbar-default col-xs-3 col-lg-2 bgcolor-secondary children">
 	<div>
 		<div class="navbar-header col-xs-12" style="padding-bottom: 25px">
 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -15,10 +15,10 @@
 			<ul class="nav navbar-nav col-xs-12">
 				<li class="section">นักเรียน</li>
 				<li id="form">
-					<a ui-sref="form.list">
+					<a ui-sref="form.list" class="bgcolor-clear children">
 						<table>
 						<tr>
-							<td><span class="fa fa-pencil-square-o"></span></td>
+							<td><span class="fa fa-pencil-square-o color-accent1"></span></td>
 							<td>แบบฟอร์ม</td>
 						</tr>
 						</table>
@@ -26,10 +26,10 @@
 				</li>
 				@if (Auth::guest())
 				<li id="login">
-					<a ui-sref="login">
+					<a ui-sref="login" class="bgcolor-clear children">
 						<table>
 						<tr>
-							<td><span class="fa fa-lock"></span></td>
+							<td><span class="fa fa-lock color-accent1"></span></td>
 							<td>เข้าสู่ระบบ</td>
 						</tr>
 						</table>
@@ -38,40 +38,40 @@
 				@else
 				<li class="section">อาจารย์</li>
 				<li id="risk-screening">
-					<a ui-sref="risk-screening">
+					<a ui-sref="risk-screening" class="bgcolor-clear children">
 						<table>
 						<tr>
-							<td><span class="fa fa-pencil-square-o"></span></td>
+							<td><span class="fa fa-pencil-square-o color-accent1"></span></td>
 							<td>แบบคัดกรอง</td>
 						</tr>
 						</table>
 					</a>
 				</li>
 				<li id="report">
-					<a ui-sref="report({type:'person'})">
+					<a ui-sref="report({type:'person'})" class="bgcolor-clear children">
 						<table>
 						<tr>
-							<td><span class="fa fa-file-text"></span></td>
+							<td><span class="fa fa-file-text color-accent1"></span></td>
 							<td>รายงาน</td>
 						</tr>
 						</table>
 					</a>
 				</li>
 				<li id="settings">
-					<a ui-sref="settings()">
+					<a ui-sref="settings()" class="bgcolor-clear children">
 						<table>
 						<tr>
-							<td><span class="fa fa-gear"></span></td>
+							<td><span class="fa fa-gear color-accent1"></span></td>
 							<td>ตั้งค่าระบบ</td>
 						</tr>
 						</table>
 					</a>
 				</li>
 				<li id="logout">
-					<a href="auth/logout">
+					<a href="auth/logout" class="bgcolor-clear children">
 						<table>
 						<tr>
-							<td><span class="fa fa-sign-out"></span></td>
+							<td><span class="fa fa-sign-out color-accent1"></span></td>
 							<td>ออกจากระบบ</td>
 						</tr>
 						</table>
--- resources/views/questionaire/main.blade.php
+++ resources/views/questionaire/main.blade.php
@@ -1,11 +1,14 @@
-<div class="container form-list">
-	<h3>เลือกแบบฟอร์มที่ต้องการใช้งาน</h3>
-	<hr />
-	<ul class="col-xs-12">
-		<li ng-repeat="questionaire in questionaires" ng-if="list.isNotRisk(questionaire)">
-			<a ui-sref="form.do({ formID: questionaire.id, form: questionaire })">
-				[[ questionaire.name ]]
-			</a>
-		</li>
-	</ul>
+<div class="col-xs-12 form-list no-pad bgcolor-secondary full-height">
+	<h3 class="col-xs-12 text-center color-accent1 no-margin std-pad theme-border-bottom">แบบฟอร์มประเมินตนเอง</h3>
+	<div class="col-xs-12 no-pad clickable bgcolor-secondary highlightable"
+		 ng-repeat="questionaire in questionaires" ng-if="list.isNotRisk(questionaire)" 
+		 ui-sref="form.do({ formID: questionaire.id, form: questionaire })">
+		<div class="col-xs-12 large-pad no-pad-bottom">
+			<div class="col-xs-12 theme-border-bottom large-pad-bottom">
+				<h4 class="text-center color-dominant">
+					[[ questionaire.name ]]
+				</h4>
+			</div>
+		</div>
+	</div>
 </div>
